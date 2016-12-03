25713da503603c6edf314bb819c250bc010e5642
Simon59830 <simon.destriez@hotmail.fr>
2016-12-02 23:00:53 -0500

    ajout d'un parametre currentCase + debut takeDecision

--- Etat_Interne/ForetEnchantee.pl
+++ Etat_Interne/ForetEnchantee.pl
@@ -6,6 +6,7 @@
 :-dynamic(monstruous/2).
 :-dynamic(riskMonstruous/2).
 :-dynamic(riskFall/2).
+:-dynamic(currentCase/2).
 
 % putrid(cooX, cooY).
 % windy(cooX, cooY).
@@ -13,19 +14,47 @@
 % border(cooX, cooY, bordureHaut,
 % bordureDroite, bordureBas, bordureGauche).
 
-takeDecisions(X):-
-	X=2.
+takeDecisions(Reponse):-
+	currentCase(CooX,_),
+	currentCase(_,CooY),
+	(   (putrid(CooX, CooY))),
+	Reponse=2.
 
 update_internal_state(CooXCurrentCase, CooYCurrentCase, Putrid, Windy, BordureDroite, BordureGauche, BordureHaut, BordureBas):-
         %Ajout de la case si la case est putride
 	(Putrid== true
-	->  asserta(putrid(CooXCurrentCase, CooYCurrentCase))),
-	%Ajout de la case si la case est venteuse
+	->  asserta(putrid(CooXCurrentCase, CooYCurrentCase)),
+	    (\+BordureHaut
+	     ->	(\+caseCovered(CooXCurrentCase-1, CooYCurrentCase)
+		->  asserta(riskMonstruous(CooXCurrentCase-1, CooYCurrentCase)))),
+	    (\+BordureDroite
+	     ->	(\+caseCovered(CooXCurrentCase, CooYCurrentCase+1)
+		->  asserta(riskMonstruous(CooXCurrentCase, CooYCurrentCase+1)))),
+	    (\+BordureBas
+	     ->	(\+caseCovered(CooXCurrentCase+1, CooYCurrentCase)
+		->  asserta(riskMonstruous(CooXCurrentCase+1, CooYCurrentCase)))),
+	    (\+BordureGauche
+	     ->	(\+caseCovered(CooXCurrentCase+1, CooYCurrentCase)
+		->  asserta(riskMonstruous(CooXCurrentCase+1, CooYCurrentCase))))),		%Ajout de la case si la case est venteuse
 	(Windy == true
-	 -> asserta(windy(CooXCurrentCase, CooYCurrentCase))),
-	%Mise � jours des bordures pour la case actuelle
+	 -> asserta(windy(CooXCurrentCase, CooYCurrentCase)),
+	    (\+BordureHaut
+	     ->	(\+caseCovered(CooXCurrentCase-1, CooYCurrentCase)
+		->  asserta(riskMonstruous(CooXCurrentCase-1, CooYCurrentCase)))),
+	    (\+BordureDroite
+	     ->	(\+caseCovered(CooXCurrentCase, CooYCurrentCase+1)
+		->  asserta(riskMonstruous(CooXCurrentCase, CooYCurrentCase+1)))),
+	    (\+BordureBas
+	     ->	(\+caseCovered(CooXCurrentCase+1, CooYCurrentCase)
+		->  asserta(riskMonstruous(CooXCurrentCase+1, CooYCurrentCase)))),
+	    (\+BordureGauche
+	     ->	(\+caseCovered(CooXCurrentCase+1, CooYCurrentCase)
+		->  asserta(riskMonstruous(CooXCurrentCase+1, CooYCurrentCase))))),
+	%Mise a jours des bordures pour la case actuelle
 	((BordureDroite == true, BordureGauche == true, BordureHaut == true, BordureBas ==true)
 	->  asserta(border(CooXCurrentCase, CooYCurrentCase, BordureHaut, BordureDroite, BordureBas, BordureGauche))),
 	%Ajout de la case actuelle dans les cases parcourues
-	asserta(caseCovered(CooXCurrentCase, CooYCurrentCase)).
+	asserta(caseCovered(CooXCurrentCase, CooYCurrentCase)),
+	retract(currentCase(_)),
+	asserta(currentCase(CooXCurrentCase, CooYCurrentCase)).
 
